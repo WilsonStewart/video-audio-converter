440592ef671976a07aadbe9cc7db9f45844a3cf2
Wilson Stewart <wilson@creativemachine.org>
2022-07-10 15:07:20 -0600

    initial

---
+++ app.js
@@ -0,0 +1,35 @@
+var FfmpegCommand = require("fluent-ffmpeg");
+var open = require("open");
+
+const convertToMp3 = (videoFilePath, audioDestinationPath) => {
+  console.log("fish");
+
+  var command = new FfmpegCommand(videoFilePath);
+
+  command.toFormat("mp3").save(audioDestinationPath);
+};
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/convert", (req, res) => {
+  convertToMp3(
+    `VIDEO/${req.query.videoFilePath}`,
+    `MP3/${req.query.outputName}.mp3`
+  );
+  res.redirect("/terminate");
+});
+
+app.get("terminate", (req, res) => {
+  res.sendStatus(200);
+  process.abort();
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+open("http://localhost:3000");
